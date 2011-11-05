dc08693028542b799e0285d3e192f842d7fc84b2
Tom Bytheway <tb403@cam.ac.uk>
2011-11-05 15:37:50 +0000

    Basic pydoc interaction.

---
+++ learning/doc.py
@@ -0,0 +1,16 @@
+"""This is an expample of pydoc docstring usage"""
+
+
+class Foo:
+	"""Classes can be documented."""
+	
+	def bar():
+		"""And their functions."""
+		pass
+
+
+def get():
+	"""Global functions aswell."""
+	pass
+
+
