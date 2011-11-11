1d01153ae04d787aeb2f213dcdf2f280270a11c2
Tom Bytheway <tb403@cam.ac.uk>
2011-11-11 12:01:01 +0000

    Additional test added to event sanitiser.

--- auditor/tests/test_event_sanitiser.py
+++ auditor/tests/test_event_sanitiser.py
@@ -14,10 +14,22 @@ class TestEventSanitiser(unittest.TestCase):
         self.evSan = event_sanitiser.EventSanitiser()
         self.evSan.registerCallback(self.callback)
 
-    def test_sucessful(self):
+    def test_basic_sanitise(self):
         event = ("Create","/home/tom","foo.py")
         self.evSan.sanitise(event)
         self.assertEqual(self.recorded,"Create:/home/tom/foo.py|")
+    
+    def test_blocked_path_addition(self):
+        path = "/home/tom"
+        events = [("Create","/media/RAID","blar.gif"),("Delete","/home/tom/doc/test","thing.pdf"),("Modify","/media/RAID","heim.frg")]
+        
+        self.evSan.addBlockedPath(path)
+        
+        for e in events:
+            self.evSan.sanitise(e)
+        
+        self.assertEqual(self.recorded,"Create:/media/RAID/blar.gif|Modify:/media/RAID/heim.frg|")
+    
 
 if __name__ == '__main__':
     unittest.main(verbosity=3)
