b1809256fab593fb6087eb52de06e9bc32475b56
Tom Bytheway <tomb34536@googlemail.com>
2011-11-21 23:44:33 +0000

    Basic file scan queue. Needs full testing.

---
+++ auditor/file_scan_queue.py
@@ -0,0 +1,31 @@
+
+class FileScanQueue():
+    def __init__(self):
+        self.queue = []
+        self.first_visit = ""
+    
+    def add(self,path):
+        for (p,f) in self.queue:
+            if(p==path):
+                self.queue.remove((p,f))
+                self.queue.append((p,True))
+                return
+        self.queue.append((path,False))
+    
+    def __next__(self):
+        if(len(self.queue)==0):
+            raise StopIteration
+        (p,f) = self.queue.pop()
+        if(p == self.first_visit):
+            self.queue.append((p,f))
+            raise StopIteration
+        if(f):
+            if(self.first_visit == ""):
+                self.first_visit = p
+            self.queue.append((p,False))
+            return self.__next__()
+        return p            
+    
+    def __iter__(self):
+        self.first_visit = ""
+        return self
