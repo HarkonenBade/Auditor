938ac4bf1585e5191e63d421a373fc79a600bb85
Tom Bytheway <tb403@cam.ac.uk>
2012-01-19 21:30:39 +0000

    Binary string encoded bug tested.

--- auditor/string_enc.py
+++ auditor/string_enc.py
@@ -12,11 +12,12 @@ def enc_str_unit(string,m_len):
     return ret
 
 def enc_str_bin(string,m_len):
+    '''Encodes a string as a series of 0's and 1's based on a binary encoding.'''
     ret = []
     for i in range(m_len):
         if(i<len(string)):
             strval = ord(string[i])
-            for k in range(8,0,-1):
+            for k in range(7,-1,-1):
                 if(strval/(2**k)>=1):
                     strval = strval-(2**k)
                     ret.extend([1])
