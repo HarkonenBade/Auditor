2208467a9db30c57dad14a58321e49b42fe1c8cf
Tom Bytheway <tb403@cam.ac.uk>
2012-01-19 11:54:42 +0000

    Simple comman processor for interface. Needs testing.

---
+++ auditor/cmdproc.py
@@ -0,0 +1,15 @@
+cmds = {}
+cmds['help'] = lambda : print('This is a help string.')
+
+
+
+def cmd_loop(cmds,prompt=">",quitstr="q"):
+    inp = input(prompt)
+    while inp != quitstr:
+        if(inp in cmds):
+            cmds[inp]()
+        else:
+            print("That is not a valid command")
+        inp = input(prompt)
+
+cmd_loop(cmds)
