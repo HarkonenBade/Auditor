0ea04219eb95b57a285ffdcf7ea094ce1a8b8f59
Tom Bytheway <tb403@cam.ac.uk>
2012-01-19 21:20:17 +0000

    Moved string encoding to its own file. Added binary encoding, needs testing.

---
+++ auditor/string_enc.py
@@ -0,0 +1,34 @@
+
+def enc_str_unit(string,m_len):
+    '''Encodes a string as a list of 0's and 1's based on a uniary encoding.'''
+    ret = []
+    for i in range(m_len):
+        if(i<len(string)):
+            ret.extend([0]*(ord(string[i])-1))
+            ret.extend([1])
+            ret.extend([0]*(128-ord(string[i])))
+        else:
+            ret.extend([0]*128)
+    return ret
+
+def enc_str_bin(string,m_len):
+    ret = []
+    for i in range(m_len):
+        if(i<len(string)):
+            strval = ord(string[i])
+            for k in range(8,0,-1):
+                if(strval/(2**k)>=1):
+                    strval = strval-(2**k)
+                    ret.extend([1])
+                else:
+                    ret.extend([0])
+    return ret
+
+def enc_str_single(string,m_len):
+    ret = []
+    for i in range(m_len):
+        if(i<len(string)):
+            ret.extend([ord(string[i])/128])
+        else:
+            ret.extend([0])
+    return ret
--- auditor/tests/test_string_enc.py
+++ auditor/tests/test_string_enc.py
@@ -1,25 +1,5 @@
 from .perceptron import Perceptron
-
-def enc_str_unit(string,m_len):
-    ret = []
-    for i in range(m_len):
-        if(i<len(string)):
-            ret.extend([0]*(ord(string[i])-1))
-            ret.extend([1])
-            ret.extend([0]*(128-ord(string[i])))
-        else:
-            ret.extend([0]*128)
-    return ret
-
-def enc_str_single(string,m_len):
-    ret = []
-    for i in range(m_len):
-        if(i<len(string)):
-            ret.extend([ord(string[i])/128])
-        else:
-            ret.extend([0])
-    return ret
-    
+from .string_enc import enc_str_unit,enc_str_single    
 
 def test():
     test_data = ["abc","def","h jk","abc","ts"]*20
