23bdabe9316eabbdbcf40b07111156a4df5813d6
Tom Bytheway <tb403@cam.ac.uk>
2012-01-20 16:00:05 +0000

    Bug tested, seems to be working ok. Needs further testing with real file interactions.

--- auditor/k_nearest_neighbour.py
+++ auditor/k_nearest_neighbour.py
@@ -1,25 +1,25 @@
-import math
+import math,sys
 
 def attrib_dist(f1,f2,pMan):
     dists = 0
-    for (n,v) in f1.attributes:
+    for n in f1.attributes:
         dist = pMan.attributes[n](f1.attributes[n],f2.attributes[n])
         dists += dist**2
     return math.sqrt(dists)
 
 def k_nearest_neighbour(fName,fDataT,pMan,k):
-    nearest_arr = [(0,'')]*k
+    nearest_arr = [(sys.maxsize,'')]*k
     curFile = fDataT.get(fName)
     for v,f in fDataT:
         if(f!=curFile):
             dist = attrib_dist(curFile,f,pMan)
             for i in range(k):
-                if(nearest_arr[i][0] < dist):
+                if(nearest_arr[i][0] > dist):
                     nearest_arr.insert(i,(dist,v))
-                    nearest_arr.pop()'''
-                    for j in range(k-1,i,-1):
-                        nearest_arr[j+1] = nearest_arr[j]
-                    nearest_arr[i] = (dist,v)'''
+                    nearest_arr.pop()
+#                    for j in range(k-1,i,-1):
+#                        nearest_arr[j+1] = nearest_arr[j]
+#                    nearest_arr[i] = (dist,v)
     majority_rec = {}
     for d,v in nearest_arr:
         if(v in majority_rec):
@@ -29,7 +29,7 @@ def k_nearest_neighbour(fName,fDataT,pMan,k):
     
     max_votes = 0
     max_key = ""
-    for k,v in majority_rec:
+    for k,v in majority_rec.items():
         if v>max_votes:
             max_votes = v
             max_key = k
