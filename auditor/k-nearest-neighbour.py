16591074241f54f262b2b4c9d4cd575b1a507f19
Tom Bytheway <tb403@cam.ac.uk>
2012-01-20 15:21:58 +0000

    Initial version of k-nearest-neighbour complete. Needs testing.

--- auditor/k-nearest-neighbour.py
+++ auditor/k-nearest-neighbour.py
@@ -15,7 +15,23 @@ def k_nearest_neighbour(fName,fDataT,pMan,k):
             dist = attrib_dist(curFile,f,pMan)
             for i in range(k):
                 if(nearest_arr[i][0] < dist):
+                    nearest_arr.insert(i,(dist,v))
+                    nearest_arr.pop()'''
                     for j in range(k-1,i,-1):
                         nearest_arr[j+1] = nearest_arr[j]
-                    nearest_arr[i] = (dist,v)
+                    nearest_arr[i] = (dist,v)'''
+    majority_rec = {}
+    for d,v in nearest_arr:
+        if(v in majority_rec):
+            majority_rec[v] += 1
+        else:
+            majority_rec[v] = 1
     
+    max_votes = 0
+    max_key = ""
+    for k,v in majority_rec:
+        if v>max_votes:
+            max_votes = v
+            max_key = k
+    
+    return max_key
