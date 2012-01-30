cfd391fbc6b23c71cc054e8df03c565867073d53
Tom Bytheway <tb403@cam.ac.uk>
2012-01-30 00:22:25 +0000

    BUGFIX: Handled case where wither string has length 0 correctly, returns length of remaining string. Also removed unneeded debugging output.

--- auditor/string_dist.py
+++ auditor/string_dist.py
@@ -3,22 +3,25 @@
 def levenshtienDist(a,b):
     m = len(a)
     n = len(b)
+
+    if m==0 or n==0:
+        return max(m,n)
+
     d = [0]*m
     for i in range(m):
         d[i] = [0]*n
-    print(d)
     
     for i in range(m):
         d[i][0] = i
     
     for j in range(n):
         d[0][j] = j
-    print(d)
+    
     for j in range(1,n):
         for i in range(1,m):
             if(a[i] == b[j]):
                 d[i][j] = d[i-1][j-1]
             else:
                 d[i][j] = min(d[i-1][j]+1,d[i][j-1]+1,d[i-1][j-1]+1)
-    print(d)
+    
     return d[m-1][n-1]
