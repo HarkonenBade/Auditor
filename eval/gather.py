f7f3d639cc938cda86a50dfbc8b6358c05d6bfe2
Tom Bytheway <tb403@cam.ac.uk>
2012-02-17 01:33:44 +0000

    FEATURE: Added gathering of average/min/max folder size, changed time output to directly produce avg and var and changed to output files suitable for gnuplot.

--- eval/gather.py
+++ eval/gather.py
@@ -1,4 +1,5 @@
 import sys,configparser,subprocess,pstats
+from auditor import file_data_tree
 
 tname = sys.argv[1]
 
@@ -9,33 +10,80 @@ klist = [int(k) for k in cfg.get("control","klist").split(":")]
 
 reps = cfg.getint("control","reps")
 
-ofile = open("%s.csv"%tname,"w")
+ofile = open("%s.dat"%tname,"w")
 
-ofile.write(",".join([""]+["%02d"%k for k in klist]) + "\n")
+#ofile.write(",".join([""]+["%02d"%k for k in klist]) + "\n")
 
-clist = ["Correct"]
-flist = ["Fail"]
-mlist = ["Miss"]
+#clist = ["Correct"]
+#flist = ["Fail"]
+#mlist = ["Miss"]
 for k in klist:
     foo = subprocess.check_output("python ../res_eval.py %s.map %s.%02d.000.log"%(tname,tname,k),shell=True)
     (total,cor,fail,miss) = foo.decode("utf-8")[:-1].split(",")
     total = int(total)
-    clist += ["%f"%(int(cor)/total)]
-    flist += ["%f"%(int(fail)/total)]
-    mlist += ["%f"%(int(miss)/total)]
+    #clist += ["%f"%(int(cor)/total)]
+    #flist += ["%f"%(int(fail)/total)]
+    #mlist += ["%f"%(int(miss)/total)]
+    ofile.write("%02d\t%f\t%f\t%f\n"%(k,int(cor)/total,int(fail)/total,int(miss)/total))
 
-ofile.write(",".join(clist) + "\n")
-ofile.write(",".join(flist) + "\n")
-ofile.write(",".join(mlist) + "\n\n\n")
+ofile.write("\n\n")
+#ofile.write(",".join(clist) + "\n")
+#ofile.write(",".join(flist) + "\n")
+#ofile.write(",".join(mlist) + "\n\n\n")
 
 
-for i in range(reps):
-	tmp = ["%2d"%i]
-	for k in klist:
-		foo = pstats.Stats("%s.%02d.%03d.log.pro"%(tname,k,i))
-		tmp += ["%f"%foo.total_tt]
-	ofile.write(",".join(tmp) + "\n")
+#for i in range(reps):
+#	tmp = ["%2d"%i]
+#	for k in klist:
+#		foo = pstats.Stats("%s.%02d.%03d.log.pro"%(tname,k,i))
+#		tmp += ["%f"%foo.total_tt]
+#	ofile.write(",".join(tmp) + "\n")
 
-ofile.close()
+for k in klist:
+    tmp = []
+    for i in range(reps):
+        foo = pstats.Stats("%s.%02d.%03d.log.pro"%(tname,k,i))
+        tmp += [foo.total_tt]
+    avg = sum(tmp)/len(tmp)
+    var = sum([t*t for t in tmp])/len(tmp) - avg*avg
+    ofile.write("%02d\t%f\t%f\n"%(k,avg,var))
 
+ofile.write("\n\n")    
+
+fd = file_data_tree.FileDataTree()
+fd.load(cfg.get("data","db_loc"),comp=False)
+
+freqs = {}
+for p,f in fd:
+    if p in freqs:
+        freqs[p] += 1
+    else:
+        freqs[p] = 1
+
+#ofile = open("%s.info"%tname,'wt')
+
+#ofile.write(",AVG FLDR,MIN FLDR,MAX FLDR\n")
+
+fval = freqs.values()
+min_folder = min(fval)
+max_folder = max(fval)
+avg_folder = sum(fval)/len(fval)
+#print(freqs)
+ofile.write("%f\t%f\t%f\n"%(avg_folder,min_folder,max_folder))
+
+
+for p,f in fd.folder_iter():
+    if p in freqs:
+        freqs[p] += 1
+    else:
+        freqs[p] = 1
+
+
+
+fval = freqs.values()
+min_folder = min(fval)
+max_folder = max(fval)
+avg_folder = sum(fval)/len(fval)
+
+ofile.write("%f\t%f\t%f"%(avg_folder,min_folder,max_folder))
 ofile.close()
