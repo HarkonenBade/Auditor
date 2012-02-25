a704dff924a2a55cb6b1fe26360baf5da361dfba
Tom Bytheway <tb403@cam.ac.uk>
2012-02-25 22:11:52 +0000

    BUGFIX: Moved test_accuracy to a more sensible location.

--- eval/run_test.py
+++ eval/run_test.py
@@ -19,13 +19,13 @@ reps = cfg.getint("control","reps")
 
 if(sys.argv[2]=="gen"):
 	with open("%s.gen.log"%tname,"w") as log:
-		subprocess.call("PYTHONPATH=/home/tom/src/stagger-read-only:/home/tom/prj python -mcProfile -o%s.gen.pro.log /home/tom/prj/auditor/tests/test_accuracy.py ../%s.conf 1 gen"%(tname,tname),shell=True,stdout=log)
+		subprocess.call("PYTHONPATH=/home/tom/src/stagger-read-only:/home/tom/prj python -mcProfile -o%s.gen.pro.log /home/tom/prj/eval/test_accuracy.py ../%s.conf 1 gen"%(tname,tname),shell=True,stdout=log)
 else:
 	for i in range(reps):
 		print("REP %03d :["%i,end="")
 		for k in klist:
 			print("*",end="")
 			with open("%s.%02d.%03d.log"%(tname,k,i),"w") as log:
-				subprocess.call("PYTHONPATH=/home/tom/src/stagger-read-only:/home/tom/prj/ python -mcProfile -o%s.%02d.%03d.log.pro /home/tom/prj/auditor/tests/test_accuracy.py ../%s.conf %02d scan"%(tname,k,i,tname,k),shell=True,stdout=log)
+				subprocess.call("PYTHONPATH=/home/tom/src/stagger-read-only:/home/tom/prj/ python -mcProfile -o%s.%02d.%03d.log.pro /home/tom/prj/eval/test_accuracy.py ../%s.conf %02d scan"%(tname,k,i,tname,k),shell=True,stdout=log)
 		print("]")
 
