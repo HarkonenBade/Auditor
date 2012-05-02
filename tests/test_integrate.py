8f63d20f4293a538724847ae5110a135d09c6b6a
Tom Bytheway <tb403@cam.ac.uk>
2012-05-02 22:53:51 +0100

    FF: Commented out non unit tests.

--- tests/test_integrate.py
+++ tests/test_integrate.py
@@ -1,20 +1,20 @@
 import unittest
 from auditor import *
+from auditor.plugins import *
 
-
-class TestIntegrate(unittest.TestCase):
-    def test_integration(self):
-        iNote = inotify_interface.INotifyInterface(100)
-        fData = file_data_tree.FileDataTree()
-        fScanQ = file_scan_queue.FileScanQueue()
-        pMan = plugin_manager.PluginManager("./test_plugins","./test_cache")
-        evHan = event_handler.EventHandler(fScanQ,fData)
-        fScan = file_scanner.FileScanner(fScanQ,fData,pMan)
-        
-        iNote.setHandler(evHan.process)
-        iNote.startWatch("./testbed")
-        
-        pMan.loadAll()
+#class TestIntegrate(unittest.TestCase):
+ #   def test_integration(self):
+  #      iNote = inotify_interface.INotifyInterface(100)
+   #     fData = file_data_tree.FileDataTree()
+    #    fScanQ = file_scan_queue.FileScanQueue()
+     #   pMan = plugin_manager.PluginManager("./test_plugins","./test_cache")
+      #  evHan = event_handler.EventHandler(fScanQ,fData)
+       # fScan = file_scanner.FileScanner(fScanQ,fData,pMan)
+        #
+#        iNote.setHandler(evHan.process)
+#        iNote.startWatch("./testbed")
+#        
+#        pMan.loadAll()
         
 
 if __name__=="__main__":
--- tests/test_string_enc.py
+++ tests/test_string_enc.py
@@ -1,10 +1,10 @@
-from auditor.perceptron import Perceptron
-from auditor.string_enc import enc_str_unit,enc_str_single    
-
-def test():
-    test_data = ["abc","def","h jk","abc","ts"]*20
-    test_rez  = [1,-1,-1,1,-1]*20
-    per = Perceptron()
-    per.init_weights(512)
-    per.train(zip(map(lambda x:enc_str_unit(x,4),test_data),test_rez))
-    return per
+#from auditor.perceptron import Perceptron
+#from auditor.string_enc import enc_str_unit,enc_str_single    #
+#
+#def test():
+#    test_data = ["abc","def","h jk","abc","ts"]*20
+#    test_rez  = [1,-1,-1,1,-1]*20
+#    per = Perceptron()
+#    per.init_weights(512)
+#    per.train(zip(map(lambda x:enc_str_unit(x,4),test_data),test_rez))
+#    return per
